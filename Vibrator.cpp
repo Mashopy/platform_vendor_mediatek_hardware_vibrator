a9e319a8b77f9370ac2b5b145a93588b338cbe1e
Mashopy <eliasgheeraert@gmail.com>
2023-05-15 01:57:54 +0200

    vibrator: Import MediaTek ALPS 12 changes
    Change-Id: I2324b3f9c1a23026a77df2b3b1034fdf2de473bf

--- Vibrator.cpp
+++ Vibrator.cpp
@@ -15,9 +15,10 @@
  */
 
 #include "vibrator-impl/Vibrator.h"
-
-#include <android-base/logging.h>
 #include <thread>
+
+#undef LOG_TAG
+#define LOG_TAG "Vibrator"
 
 namespace aidl {
 namespace android {
@@ -27,7 +28,6 @@ namespace vibrator {
 static constexpr int32_t kComposeDelayMaxMs = 1000;
 static constexpr int32_t kComposeSizeMax = 256;
 static constexpr int32_t kComposePwleSizeMax = 127;
-
 static constexpr float kResonantFrequency = 150.0;
 static constexpr float kQFactor = 11.0;
 static constexpr int32_t COMPOSE_PWLE_PRIMITIVE_DURATION_MAX_MS = 16383;
@@ -36,45 +36,214 @@ static constexpr float PWLE_LEVEL_MAX = 0.98256;
 static constexpr float PWLE_FREQUENCY_RESOLUTION_HZ = 1.0;
 static constexpr float PWLE_FREQUENCY_MIN_HZ = 140.0;
 static constexpr float PWLE_FREQUENCY_MAX_HZ = 160.0;
+// Use effect #1 in the waveform library for CLICK effect
+static constexpr int32_t WAVEFORM_CLICK_EFFECT = 30;
+
+// Use effect #2 in the waveform library for TICK effect
+static constexpr int32_t WAVEFORM_TICK_EFFECT = 50;
+
+// Use effect #3 in the waveform library for DOUBLE_CLICK effect
+static constexpr int32_t WAVEFORM_DOUBLE_CLICK_EFFECT = 70;
+
+// Use effect #4 in the waveform library for HEAVY_CLICK effect
+static constexpr int32_t WAVEFORM_HEAVY_CLICK_EFFECT = 90;
+
+
+#define TIMEOUT_STR_LEN         20
+
+static const char THE_DEVICE[] = "/sys/class/timed_output/vibrator/enable";
+
+static bool device_exists(const char *file) {
+    int fd = -1;
+
+    fd = TEMP_FAILURE_RETRY(open(file, O_RDWR));
+    if(fd < 0) {
+        return false;
+    }
+
+    close(fd);
+    fd = -1;
+    return true;
+}
+
+static bool vibra_exists() {
+    return device_exists(THE_DEVICE);
+}
+
+static int write_value(const char *file, const char *value)
+{
+    int to_write = 0, written = -1, ret = 0;
+    int fd = -1;
+
+    fd = TEMP_FAILURE_RETRY(open(file, O_WRONLY));
+    if (fd < 0) {
+        return -errno;
+    }
+
+    to_write = strlen(value) + 1;
+    written = TEMP_FAILURE_RETRY(write(fd, value, to_write));
+    if (written == -1) {
+        ret = -errno;
+    } else if (written != to_write) {
+        /* even though EAGAIN is an errno value that could be set
+           by write() in some cases, none of them apply here.  So, this return
+           value can be clearly identified when debugging and suggests the
+           caller that it may try to call vibrator_on() again */
+        ret = -EAGAIN;
+    } else {
+        ret = 0;
+    }
+
+    errno = 0;
+    close(fd);
+    fd = -1;
+
+    return ret;
+}
+
+static int sendit(unsigned int timeout_ms)
+{
+    char value[TIMEOUT_STR_LEN]; /* large enough for millions of years */
+
+    int bytes = snprintf(value, sizeof(value), "%u", timeout_ms);
+    if (bytes >= sizeof(value)) return -EINVAL;
+    return write_value(THE_DEVICE, value);
+}
+
+static int vibra_on(unsigned int timeout_ms)
+{
+    /* constant on, up to maximum allowed time */
+    return sendit(timeout_ms);
+}
+
+static int vibra_off()
+{
+    return sendit(0);
+}
+
+static const char LED_DEVICE[] = "/sys/class/leds/vibrator";
+
+static int write_led_file(const char *file, const char *value)
+{
+    char file_str[50];
+
+    int bytes = snprintf(file_str, sizeof(file_str), "%s/%s", LED_DEVICE, file);
+    if (bytes >= sizeof(file_str)) return -EINVAL;
+    return write_value(file_str, value);
+}
+
+static bool vibra_led_exists()
+{
+    char file_str[50];
+
+    int bytes = snprintf(file_str, sizeof(file_str), "%s/%s", LED_DEVICE, "activate");
+    if (bytes >= sizeof(file_str)) return -EINVAL;
+    return device_exists(file_str);
+}
+
+static int vibra_led_on( unsigned int timeout_ms)
+{
+    int ret;
+    char value[TIMEOUT_STR_LEN]; /* large enough for millions of years */
+
+    ret = write_led_file("state", "1");
+    if (ret)
+        return ret;
+
+    int bytes = snprintf(value, sizeof(value), "%u\n", timeout_ms);
+    if (bytes >= sizeof(value)) return -EINVAL;
+    ret = write_led_file("duration", value);
+    if (ret)
+        return ret;
+
+    return write_led_file("activate", "1");
+}
+
+static int vibra_led_off()
+{
+    return write_led_file("activate", "0");
+}
 
 ndk::ScopedAStatus Vibrator::getCapabilities(int32_t* _aidl_return) {
     LOG(INFO) << "Vibrator reporting capabilities";
+    if (_aidl_return == nullptr) {
+        return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_ILLEGAL_ARGUMENT));
+    }
+#ifndef VIBR_EFFECT_SUPPORT
+    *_aidl_return = 0;
+#else
     *_aidl_return = IVibrator::CAP_ON_CALLBACK | IVibrator::CAP_PERFORM_CALLBACK |
                     IVibrator::CAP_AMPLITUDE_CONTROL | IVibrator::CAP_EXTERNAL_CONTROL |
                     IVibrator::CAP_EXTERNAL_AMPLITUDE_CONTROL | IVibrator::CAP_COMPOSE_EFFECTS |
                     IVibrator::CAP_ALWAYS_ON_CONTROL | IVibrator::CAP_GET_RESONANT_FREQUENCY |
                     IVibrator::CAP_GET_Q_FACTOR | IVibrator::CAP_FREQUENCY_CONTROL |
                     IVibrator::CAP_COMPOSE_PWLE_EFFECTS;
+#endif
+
     return ndk::ScopedAStatus::ok();
 }
 
 ndk::ScopedAStatus Vibrator::off() {
-    LOG(INFO) << "Vibrator off";
+    ALOGD("Vibrator off");
+    if (vibra_exists()) {
+        ALOGD("Vibrator using timed_output");
+        vibra_off();
+    } else if (vibra_led_exists()) {
+        ALOGD("Vibrator using LED trigger");
+        vibra_led_off();
+    } else {
+        ALOGI("Vibrator device does not exist. Cannot start vibrator");
+    }
     return ndk::ScopedAStatus::ok();
 }
 
 ndk::ScopedAStatus Vibrator::on(int32_t timeoutMs,
                                 const std::shared_ptr<IVibratorCallback>& callback) {
-    LOG(INFO) << "Vibrator on for timeoutMs: " << timeoutMs;
+    ALOGI("Vibrator on for timeoutMs: %d", timeoutMs);
+
+    if (vibra_exists()) {
+        ALOGD("Vibrator using timed_output");
+        vibra_on(timeoutMs);
+    } else if (vibra_led_exists()) {
+        ALOGD("Vibrator using LED trigger");
+        vibra_led_on(timeoutMs);
+    } else {
+        ALOGI("Vibrator device does not exist. Cannot start vibrator");
+    }
     if (callback != nullptr) {
+#ifndef VIBR_EFFECT_SUPPORT
+        return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
         std::thread([=] {
-            LOG(INFO) << "Starting on on another thread";
+            ALOGD("Starting on on another thread");
             usleep(timeoutMs * 1000);
-            LOG(INFO) << "Notifying on complete";
+            ALOGD("Notifying on complete");
             if (!callback->onComplete().isOk()) {
-                LOG(ERROR) << "Failed to call onComplete";
+                ALOGI("Failed to call onComplete");
             }
         }).detach();
-    }
     return ndk::ScopedAStatus::ok();
+#endif
+    } else {
+        return ndk::ScopedAStatus::ok();
+    }
+
 }
 
 ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength strength,
                                      const std::shared_ptr<IVibratorCallback>& callback,
                                      int32_t* _aidl_return) {
-    LOG(INFO) << "Vibrator perform";
+    uint32_t timeMS = 0;
+    ndk::ScopedAStatus status;
 
-    if (effect != Effect::CLICK && effect != Effect::TICK) {
+    ALOGD("Vibrator perform %d, %d", (int)strength, (int)effect);
+    if (callback != nullptr) {
+        return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+    }
+
+    if (effect != Effect::CLICK && effect != Effect::TICK
+        && effect != Effect::TEXTURE_TICK && effect != Effect::DOUBLE_CLICK
+        && effect != Effect::HEAVY_CLICK) {
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
     }
     if (strength != EffectStrength::LIGHT && strength != EffectStrength::MEDIUM &&
@@ -82,37 +251,62 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength strength,
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
     }
 
-    constexpr size_t kEffectMillis = 100;
-
-    if (callback != nullptr) {
-        std::thread([=] {
-            LOG(INFO) << "Starting perform on another thread";
-            usleep(kEffectMillis * 1000);
-            LOG(INFO) << "Notifying perform complete";
-            callback->onComplete();
-        }).detach();
+    switch (effect) {
+        case Effect::TEXTURE_TICK:
+            timeMS = WAVEFORM_TICK_EFFECT;
+            break;
+        case Effect::CLICK:
+            timeMS = WAVEFORM_CLICK_EFFECT;
+            break;
+        case Effect::DOUBLE_CLICK:
+            timeMS = WAVEFORM_DOUBLE_CLICK_EFFECT;
+            break;
+        case Effect::TICK:
+            timeMS = WAVEFORM_TICK_EFFECT;
+            break;
+        case Effect::HEAVY_CLICK:
+            timeMS = WAVEFORM_HEAVY_CLICK_EFFECT;
+            break;
+        default:
+           return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
     }
 
-    *_aidl_return = kEffectMillis;
-    return ndk::ScopedAStatus::ok();
+    status = on(timeMS, nullptr);
+    if (!status.isOk()) {
+        return status;
+    } else {
+        *_aidl_return = timeMS;
+        return ndk::ScopedAStatus::ok();
+    }
 }
 
 ndk::ScopedAStatus Vibrator::getSupportedEffects(std::vector<Effect>* _aidl_return) {
-    *_aidl_return = {Effect::CLICK, Effect::TICK};
+
+    *_aidl_return = {Effect::CLICK, Effect::TICK, Effect::TEXTURE_TICK,
+        Effect::DOUBLE_CLICK, Effect::HEAVY_CLICK};
+
     return ndk::ScopedAStatus::ok();
 }
 
 ndk::ScopedAStatus Vibrator::setAmplitude(float amplitude) {
     LOG(INFO) << "Vibrator set amplitude: " << amplitude;
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     if (amplitude <= 0.0f || amplitude > 1.0f) {
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_ILLEGAL_ARGUMENT));
     }
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::setExternalControl(bool enabled) {
     LOG(INFO) << "Vibrator set external control: " << enabled;
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::getCompositionDelayMax(int32_t* maxDelayMs) {
@@ -126,6 +320,9 @@ ndk::ScopedAStatus Vibrator::getCompositionSizeMax(int32_t* maxSize) {
 }
 
 ndk::ScopedAStatus Vibrator::getSupportedPrimitives(std::vector<CompositePrimitive>* supported) {
+#ifndef VIBR_EFFECT_SUPPORT
+    *supported = {};
+#else
     *supported = {
             CompositePrimitive::NOOP,       CompositePrimitive::CLICK,
             CompositePrimitive::THUD,       CompositePrimitive::SPIN,
@@ -133,16 +330,12 @@ ndk::ScopedAStatus Vibrator::getSupportedPrimitives(std::vector<CompositePrimiti
             CompositePrimitive::QUICK_FALL, CompositePrimitive::LIGHT_TICK,
             CompositePrimitive::LOW_TICK,
     };
+#endif
     return ndk::ScopedAStatus::ok();
 }
 
 ndk::ScopedAStatus Vibrator::getPrimitiveDuration(CompositePrimitive primitive,
                                                   int32_t* durationMs) {
-    std::vector<CompositePrimitive> supported;
-    getSupportedPrimitives(&supported);
-    if (std::find(supported.begin(), supported.end(), primitive) == supported.end()) {
-        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-    }
     if (primitive != CompositePrimitive::NOOP) {
         *durationMs = 100;
     } else {
@@ -220,22 +413,38 @@ ndk::ScopedAStatus Vibrator::alwaysOnDisable(int32_t id) {
 
 ndk::ScopedAStatus Vibrator::getResonantFrequency(float *resonantFreqHz) {
     *resonantFreqHz = kResonantFrequency;
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::getQFactor(float *qFactor) {
     *qFactor = kQFactor;
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::getFrequencyResolution(float *freqResolutionHz) {
     *freqResolutionHz = PWLE_FREQUENCY_RESOLUTION_HZ;
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::getFrequencyMinimum(float *freqMinimumHz) {
     *freqMinimumHz = PWLE_FREQUENCY_MIN_HZ;
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::getBandwidthAmplitudeMap(std::vector<float> *_aidl_return) {
@@ -243,17 +452,29 @@ ndk::ScopedAStatus Vibrator::getBandwidthAmplitudeMap(std::vector<float> *_aidl_
     //     (PWLE_FREQUENCY_MAX_HZ - PWLE_FREQUENCY_MIN_HZ) / PWLE_FREQUENCY_RESOLUTION_HZ
     *_aidl_return = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10,
                      0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20};
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::getPwlePrimitiveDurationMax(int32_t *durationMs) {
     *durationMs = COMPOSE_PWLE_PRIMITIVE_DURATION_MAX_MS;
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::getPwleCompositionSizeMax(int32_t *maxSize) {
     *maxSize = kComposePwleSizeMax;
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 ndk::ScopedAStatus Vibrator::getSupportedBraking(std::vector<Braking> *supported) {
@@ -261,7 +482,11 @@ ndk::ScopedAStatus Vibrator::getSupportedBraking(std::vector<Braking> *supported
             Braking::NONE,
             Braking::CLAB,
     };
+#ifndef VIBR_EFFECT_SUPPORT
+    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
+#else
     return ndk::ScopedAStatus::ok();
+#endif
 }
 
 void resetPreviousEndAmplitudeEndFrequency(float &prevEndAmplitude, float &prevEndFrequency) {
